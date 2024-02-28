b0fd568a787e57647728c199817098fa18047c60
mariana <marianass712@gmail.com>
2024-02-28 18:24:12 -0300

    se realizo el try

--- src/App.jsx
+++ src/App.jsx
@@ -6,37 +6,42 @@ import Frase from "./components/Frase";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [frasePersonaje, setFrasePersonaje] = useState({})
+  const [frasePersonaje, setFrasePersonaje] = useState({});
 
-  useEffect(()=>{
+  useEffect(() => {
     consultarAPI(); //llamada a la funcion
   }, []);
 
-  const consultarAPI = async()=>{ //async asincrona para que funcione el await
-      try{
-        const respuesta = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes'); //solicitud a la api await espera que haga la solicitud
-        const datos = await respuesta.json() //extrae el body de la respuesta, json() es un metodo que extrae body sin el formato json como objeto
-        console.log(respuesta);
-        console.log(datos);
-        //guardar los datos del api en el state
-        setFrasePersonaje(datos[0])
-      }catch(error){
-        console.log(error);
-        // agregar un mensaje para el usuario
-      } 
-
- 
-  }
+  const consultarAPI = async () => {
+    //async asincrona para que funcione el await
+    try {
+      //es basicamente una estructura para controlar la ejecucion de mi codigo
+      const respuesta = await fetch(
+        "https://thesimpsonsquoteapi.glitch.me/quotes"
+      ); //solicitud a la api await espera que haga la solicitud
+      const datos = await respuesta.json(); //extrae el body de la respuesta, json() es un metodo que extrae body sin el formato json como objeto
+      console.log(respuesta);
+      console.log(datos);
+      //guardar los datos del api en el state
+      setFrasePersonaje(datos[0]);
+    } catch (error) {
+      //si el codigo fallo en alguna linea lleva a esta parte la variable error guarda que el error que se produjo
+      console.log(error);
+      // agregar un mensaje para el usuario
+    }
+  };
   //aqui envia al componente la frase mediante el props
   return (
     <>
-       <Container className="text-center my-5">
+      <Container className="text-center my-5">
         <img src={logo} alt="Logo de los simpson" className="w-50" />
-       <Frase frasePersonajeProps={frasePersonaje}></Frase>
-        <Button variant="warning" onClick={consultarAPI}>Obtener frase</Button>
+        <Frase frasePersonajeProps={frasePersonaje}></Frase>
+        <Button variant="warning" onClick={consultarAPI}>
+          Obtener frase
+        </Button>
       </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
